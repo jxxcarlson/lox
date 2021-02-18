2dced059e840aadbb633272068d099c494071204
James Carlson <jxxcarlson@mac.com>
2021-02-18 10:13:53 -0500

    Update ParserHelpers.hs

--- src/ParserHelpers.hs
+++ src/ParserHelpers.hs
@@ -4,12 +4,16 @@ import MiniParsec
 
 type Parser = MPParser Char ParseError
 
+-- PREDICATES
+
 isAlpha' :: Char -> Bool
 isAlpha' c = c `elem` "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ_"
 
 isAlphaNum' :: Char -> Bool
 isAlphaNum' c = c `elem` "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ_0123456789"
 
+isSpace :: Char -> Bool
+isSpace c = c == ' '
 
 isNonzeroDigit :: Char -> Bool 
 isNonzeroDigit c = c `elem` "123456789"
@@ -19,12 +23,34 @@ isDigit c = c `elem` "0123456789"
 
 digit  = satisfy "digit" isDigit
 
+-- SPACE
+
+space  = satisfy "space" isSpace
+spaces = many space
+spaces' = space <* spaces
+
+-- SYMBOL
+
+symbol s = string (s ++ " ") <* spaces
+symbol' s = string s <* spaces
+
+char c = satisfy [c]     (== c)
+string = traverse char
+
+-- NUMBERS
+
 nonzeroDigit = (\x -> [x]) <$> satisfy "nonzero digit" isNonzeroDigit 
 
 digits = many digit
 
 decimalPoint =  string "."
 
+doubleDigits = choice "double" [try floatDigits_, integerDigits]
+
+double :: Parser Double
+double = (\x -> read x) <$> doubleDigits
+
+-- STRINGS
 
 quotationMark :: Parser String
 quotationMark = string "\""
@@ -32,7 +58,7 @@ quotationMark = string "\""
 restOfString :: Parser String
 restOfString = many (satisfy "restOfString" (\c -> c /= '\"'))
 
-foo = satisfy "foo" (\c -> c /= ' ')
+-- LITERAL STRINGS
 
 literalString = quotationMark >> restOfString <*quotationMark
 
@@ -40,10 +66,7 @@ integerDigits = (mpSequence [nonzeroDigit, digits]) <* spaces
 
 floatDigits_ = (mpSequence [integerDigits, decimalPoint, digits]) <* spaces
 
-doubleDigits = choice "double" [try floatDigits_, integerDigits]
-
-double :: Parser Double
-double = (\x -> read x) <$> doubleDigits
+-- IDENTIFIER
 
 identifier = (mpSequence [alpha', alphaNums']) <* spaces
 
@@ -56,12 +79,4 @@ alphaNum' = satisfy "alphaNum'" isAlphaNum'
 alphaNums' :: Parser String
 alphaNums' = (many alphaNum') <* spaces
 
-isSpace :: Char -> Bool
-isSpace c = c == ' '
-char c = satisfy [c]     (== c)
-string = traverse char
-space  = satisfy "space" isSpace
-spaces = many space
-spaces' = space <* spaces
-symbol s = string (s ++ " ") <* spaces
-symbol' s = string s <* spaces
+
